7680f24129a21d8341620161809413cee3faf83b
Yaroslav Pershin <62902094+mvth027@users.noreply.github.com>
2021-02-17 18:12:05 +0300

    Update dhcp_pools_distinct.py

--- dhcp_pools_distinct.py
+++ dhcp_pools_distinct.py
@@ -1,41 +1,11 @@
 import re
 
 dhcpPool = '''
-ip dhcp pool HEX_WIFI.KIROV.KB.HLYNOV.Urickogo40
- vrf HEX-WIFI
- network 100.68.3.0 255.255.255.224
- dns-server 213.87.2.88 213.87.2.89 
- default-router 100.68.3.1 
- lease 0 0 8
-ip dhcp pool HEX_WIFI.KIROV.KB.HLYNOV.Vorovskogo135
- vrf HEX-WIFI
- network 100.68.2.192 255.255.255.224
- dns-server 213.87.2.88 213.87.2.89 
- default-router 100.68.2.193 
- lease 0 0 8
-ip dhcp pool HEX_WIFI.KIROV.KB.HLYNOV.Oktyabskiy155
- vrf HEX-WIFI
- network 100.68.3.64 255.255.255.224
- dns-server 213.87.2.88 213.87.2.89 
- default-router 100.68.3.65 
- lease 0 0 8
+
 '''
 
 interfaces = '''
-Port-channel1.15201401 100.68.3.129    YES manual up                    up      
-Port-channel1.15201409 100.68.3.33     YES manual up                    up      
-Port-channel1.15201413 100.68.3.225    YES manual up                    up      
-Port-channel1.15201447 100.68.3.161    YES manual up                    up      
-Port-channel1.15201456 100.68.3.193    YES manual up                    up      
-Port-channel1.15201701 100.68.3.97     YES manual up                    up      
-Port-channel1.15203454 100.68.3.1      YES manual up                    up      
-Port-channel1.15203496 100.68.3.65     YES manual up                    up
-Port-channel1.15201465 100.68.2.97     YES NVRAM  up                    up      
-Port-channel1.15201493 100.68.2.65     YES manual up                    up      
-Port-channel1.15201704 100.68.2.1      YES NVRAM  up                    up      
-Port-channel1.15201712 100.68.2.225    YES manual up                    up      
-Port-channel1.15201716 100.68.2.129    YES NVRAM  up                    up      
-Port-channel1.15203495 100.68.2.193    YES manual up                    up
+
 '''
 
 poolNameFind = ('ip dhcp pool (\S+)')
